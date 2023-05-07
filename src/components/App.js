fee15c16db775a2158c91d54255ca6062ee020e2
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-05-07 16:12:44 +0530

    fetch data from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { useEffect,useState } from "react";
 const App = () => {
+
+  let[info,setInfo]=useState([]);
+  console.log("info",info)
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response)=>response.json()) 
+    .then((data)=>{
+      console.log("data",data)
+      setInfo(data)
+    })
+    .catch((err)=>console.log(err))
+   },[])
   return (
     <div>
-        {/* Do not remove the main div */}
+        {
+            info.map((value,index)=>(
+              <div key={index}>
+              
+                <h1>{value.id}. {value.title}</h1>
+                <p>{value.body}</p>
+              </div>
+            ))
+        }
     </div>
   )
 }
